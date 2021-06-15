a2f8d2f3c3f1b94da64901964e405fe761952f79
DenisaSeran00 <denisa.seran00@e-utv.ro>
2021-06-16 00:41:04 +0300

    .

--- src/MainView.java
+++ src/MainView.java
@@ -1,9 +1,19 @@
+import com.sun.glass.ui.Screen;
+import javafx.geometry.Point2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.DrawMode;
 import javafx.scene.transform.Affine;
+import javafx.scene.transform.NonInvertibleTransformException;
+
+import java.awt.event.MouseEvent;
+import java.sql.SQLOutput;
 
 public class MainView extends VBox {
     private Button button;
@@ -12,6 +22,8 @@ public class MainView extends VBox {
     private Affine affine;
 
     private Simulation simulation;
+    private int drawMode  = 1;
+
 
     public MainView() {
          this.button = new Button("Step");
@@ -24,6 +36,9 @@ public class MainView extends VBox {
          this.canvas= new Canvas();
          this.canvas.setHeight(750);
          this.canvas.setWidth(1500);
+         this.canvas.setOnMousePressed(this::handleDraw);
+         this.canvas.setOnMouseDragged(this::handleDraw);
+         this.setOnKeyPressed(this::onKeyPressed);
 
          this.getChildren().addAll(this.button, this.canvas);
 
@@ -31,16 +46,41 @@ public class MainView extends VBox {
          this.affine.appendScale(1500/50f,1500/50f);
 
          this.simulation = new Simulation(200 ,200);
-         simulation.setAlive(1, 2);
-         simulation.setAlive(2, 3);
-         simulation.setAlive(3,5);
-         simulation.setAlive(4, 4);
 
-         simulation.setAlive(5, 5);
-         simulation.setAlive(5,6);
-         simulation.setAlive(6, 5);
-         simulation.setAlive(6,6);
     }
+
+    private void onKeyPressed(KeyEvent keyEvent) {
+        if(keyEvent.getCode()== KeyCode.D){
+            this.drawMode = 1;
+            System.out.println("Draw Mode");
+
+        }else if(keyEvent.getCode()==KeyCode.E){
+            this.drawMode = 0;
+            System.out.println("Erase Mode");
+        }
+    }
+
+    private void handleDraw(javafx.scene.input.MouseEvent mouseEvent) {
+        double mouseX = mouseEvent.getX();
+        double mouseY = mouseEvent.getY();
+
+        System.out.println(mouseX + ","+ mouseY);
+
+        try {
+            Point2D coord = this.affine.inverseTransform(mouseX, mouseY);
+            int simX = (int) coord.getX();
+            int simY = (int) coord.getY();
+
+            System.out.print(simX + "," + simY);
+
+            this.simulation.board[simX][simY]=drawMode;
+            draw();
+
+        }catch (NonInvertibleTransformException e){
+            System.out.println("Not inverted");
+        }
+    }
+
     public void draw() {
         GraphicsContext g = this.canvas.getGraphicsContext2D();
         g.setTransform(this.affine);
